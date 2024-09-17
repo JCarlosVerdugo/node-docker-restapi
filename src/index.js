b3d2d411fa399941d2b9d6ddf57e768067fa0234
Carlos Verdugo <carlosverdugo@proton.me>
2024-09-16 19:33:55 -0700

    Adding async function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const app = require('./app');
 
-app.listen(3000, () => console.log('Server on port 3000'))
+const main = async () => {
+    app.listen(3000);
+    console.log('Server is running')
+}
+
+main();
+
+// app.listen(3000, () => console.log('Server on port 3000'))
